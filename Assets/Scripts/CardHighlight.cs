ce13a6bc91dc0e17606b7835bdedb4dee4ee27cc
Beatriz de Oliveira <beatrizopiedade@outlook.com>
2024-12-23 18:01:40 -0300

    teste

---
+++ Assets/Scripts/CardHighlight.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CardHighlight : MonoBehaviour {
+    [SerializeField] private RectTransform _card;
+    private int originalIdx;
+    private Vector3 originalPos;
+    private float offset = 10.0f;
+
+    public void MouseEnter() {
+        originalIdx = _card.GetSiblingIndex();
+        originalPos = _card.localPosition;
+
+        _card.SetAsLastSibling();
+        _card.localPosition = originalPos + Vector3.up * offset;
+    }
+
+    public void MouseExit() {
+        _card.SetSiblingIndex(originalIdx);
+        _card.localPosition = originalPos;
+    }
+}

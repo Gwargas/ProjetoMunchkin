4e58017b2a083184bdf262b5673c8c09550752b6
David Dias <58055711+DavidDiasLugon@users.noreply.github.com>
2024-12-31 14:59:43 -0300

    Adição de atributos e métodos
    Atributos e métodos presentes no diagrama que faltavam no script.

--- Assets/Scripts/CartasScript/CartaEquipamento.cs
+++ Assets/Scripts/CartasScript/CartaEquipamento.cs
@@ -3,6 +3,15 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CartaEquipamento", menuName = "Scriptable Objects/CartaEquipamento")]
 public class CartaEquipamento : CartaTesouro
 {
+    [SerializedField] private int ehGrande;
+    [SerializedField] private string parteCorpo;
+    [SerializedField] private string limitacaoRaca; 
+    [SerializedField] private string limitacaoClasse;
+
+    public bool condicaoUso(){
+        // (Note: David) Lógica de uso 
+    } 
+
     public override void EfeitoCompra()
     {
         throw new System.NotImplementedException();
